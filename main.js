030bcf3cb6c848b8081fcfa4dfd2d8d7e1e2c410
Jordi <jordilarareyes@gmail.com>
2024-04-06 11:31:47 +0200

    Basic structure of the program

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function addNewToDO() {
+
+}
+
+function deleteToDo() {
+
+}
+
+
+
+
+document.getElementById('btn_addNewToDO').addEventListener('click', addNewToDO);
+document.getElementById('btn_deleteToDo').addEventListener('click', deleteToDo);
