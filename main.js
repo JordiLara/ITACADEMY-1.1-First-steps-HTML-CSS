a2c5bef6ce0339bee55f9ddf3c2100c0efa6aaa2
Jordi <jordilarareyes@gmail.com>
2024-04-10 21:41:03 +0200

    deleted a .value to fix the issue where the list of new task were not shown propertly.

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 "use strict";
 
-import { ToDo } from "./Task.js";
+//import { ToDo } from "./Task.js";
 
 // add a new task to the ToDo List
 
 function addTask() {
 
-    let task = document.getElementById('task').value;
+    let task = document.getElementById('task');
     let taskList = document.getElementById('taskList');
 
-    const TASK = new ToDo(task); // Manel, no le veo el sentido al archivo Task
+    //const TASK = new ToDo(task); // Manel, no le veo el sentido al archivo Task
 
     // Generate a new element at the HTLM to show the added new task, and add a new row everytime a new task is added.
     let list = document.createElement('list'); 
